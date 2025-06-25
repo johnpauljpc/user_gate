6cb56e37d80a6c71379e1d1f1ade6820863ca249
johnpauljpc <jpcwork081@gmail.com>
2025-06-25 16:17:30 +0100

    custom user model created

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email']
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+   
+    username = models.CharField(max_length=100, verbose_name="name")
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
